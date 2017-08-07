203281bd474f0459b2370bb2e2dc720980c71e5c
yoonkt200@gmail.com <yoon@yungitaeui-MacBook-Pro.local>
2017-08-07 16:20:43 +0900

    asdf

--- chatbot/answer/views.py
+++ chatbot/answer/views.py
@@ -18,14 +18,9 @@ def keyboard(request):
 
 @csrf_exempt
 def answer(request):
-    json_str = ((request.body).decode('utf-8'))
-    received_json_data = json.loads(json_str)
-    cafeteria_name = received_json_data['content']
-    today_date = datetime.date.today().strftime("%m월 %d일")
-
     return JsonResponse({
         'message': {
-            'text': today_date + '의 ' + cafeteria_name + ' 중식 메뉴입니다.'
+            'text': ' 중식 메뉴입니다.'
         },
         'keyboard': {
             'type': 'buttons',
