dd9ebf627b595073dca30c24e00bfe98bc2404d4
yoonkt200@gmail.com <yoon@yungitaeui-MacBook-Pro.local>
2017-08-07 14:32:06 +0900

    asdf

--- chatbot/answer/views.py
+++ chatbot/answer/views.py
@@ -16,38 +16,38 @@ def keyboard(request):
     })
 
 
-# @csrf_exempt
-# def message(request):
-#     if request.POST:
-#         json_str = ((request.body).decode('utf-8'))
-#         json_data = json.loads(json_str)
-#
-#         # 응답타입 체크 : content가 "삼성", "엘지" 이런거라면 check_type = maker
-#         # 코드로는 ex) check_is_maker(user_response)
-#         # user_response = json_data['content']
-#         # def check_is_maker(user_response)
-#         #   maker = Test.objects.all().value??()['test']
-#         #   for user_response in maker:
-#         #       if maker:
-#         #           return maker
-#
-#         # if check_is_maker(user_response)
-#         # elif check_is_model(user_response)
-#         # ...
-#
-#         user_response = json_data['content']
-#         test = Test.objects.all().first()
-#         return JsonResponse({
-#             'message': {
-#                 'text': user_response
-#             },
-#             'keyboard': {
-#                 'type': 'buttons',
-#                 'buttons': ['galaxy', 'bega', 'sony']
-#             },
-#             "photo": {
-#                 "url": test.testPhoto.url,
-#                 "width": 640,
-#                 "height": 480
-#             },
-#         })
+@csrf_exempt
+def message(request):
+    if request.POST:
+        json_str = ((request.body).decode('utf-8'))
+        json_data = json.loads(json_str)
+
+        # 응답타입 체크 : content가 "삼성", "엘지" 이런거라면 check_type = maker
+        # 코드로는 ex) check_is_maker(user_response)
+        # user_response = json_data['content']
+        # def check_is_maker(user_response)
+        #   maker = Test.objects.all().value??()['test']
+        #   for user_response in maker:
+        #       if maker:
+        #           return maker
+
+        # if check_is_maker(user_response)
+        # elif check_is_model(user_response)
+        # ...
+
+        user_response = json_data['content']
+        test = Test.objects.all().first()
+        return JsonResponse({
+            'message': {
+                'text': user_response
+            },
+            'keyboard': {
+                'type': 'buttons',
+                'buttons': ['galaxy', 'bega', 'sony']
+            },
+            "photo": {
+                "url": test.testPhoto.url,
+                "width": 640,
+                "height": 480
+            },
+        })
