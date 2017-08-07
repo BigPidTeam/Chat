a7491039fa7716f24ab1c99dc9e44f9f3d332b02
yoonkt200@gmail.com <yoon@yungitaeui-MacBook-Pro.local>
2017-08-08 00:07:39 +0900

    dad

--- chatbot/chatbot/settings.py
+++ chatbot/chatbot/settings.py
@@ -40,13 +40,11 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.csrf.CsrfResponseMiddleware',
 ]
 
 ROOT_URLCONF = 'chatbot.urls'
--- chatbot/chatbot/wsgi.py
+++ chatbot/chatbot/wsgi.py
@@ -7,11 +7,7 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
-import os, sys
-
-sys.path.append('/home/ubuntu/Chat/chatbot')
-sys.path.append('/home/ubuntu/chatenv/lib/python3.5/site-packages')
-
+import os
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "chatbot.settings")
