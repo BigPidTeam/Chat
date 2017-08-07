fa3b86cbea17ef1deec0e1ab175aaf6ac0162f9f
yoonkt200@gmail.com <yoon@yungitaeui-MacBook-Pro.local>
2017-08-08 00:47:27 +0900

    asdf

--- chatbot/answer/views.py
+++ chatbot/answer/views.py
@@ -12,28 +12,28 @@ def keyboard(request):
     })
 
 
-# @csrf_exempt
-# def message(request):
-#     message = ((request.body).decode('utf-8'))
-#     return_json_str = json.loads(message)
-#     return_str = return_json_str['content']
-#
-#     return JsonResponse({
-#         'message': {
-#             'text': "you type " + return_str + "!"
-#         },
-#         'keyboard': {
-#             'type': 'buttons',
-#             'buttons': ['1', '2']
-#         }
-#     })
-#
-
 @csrf_exempt
 def message(request):
-    if request.POST:
-        json_str = ((request.body).decode('utf-8'))
-        json_data = json.loads(json_str)
+    message = ((request.body).decode('utf-8'))
+    return_json_str = json.loads(message)
+    return_str = return_json_str['content']
+    test = Test.objects.all().first()
+
+    return JsonResponse({
+        'message': {
+                'text': return_str
+            },
+            'keyboard': {
+                'type': 'buttons',
+                'buttons': ['galaxy', 'bega', 'sony']
+            },
+            "photo": {
+                "url": test.testPhoto.url,
+                "width": 640,
+                "height": 480
+            }
+    })
+
 
         # 응답타입 체크 : content가 "삼성", "엘지" 이런거라면 check_type = maker
         # 코드로는 ex) check_is_maker(user_response)
@@ -47,19 +47,3 @@ def message(request):
         # if check_is_maker(user_response)
         # elif check_is_model(user_response)
         # ...
-        test = Test.objects.all().first()
-        user_response = json_data['content']
-        return JsonResponse({
-            'message': {
-                'text': user_response
-            },
-            'keyboard': {
-                'type': 'buttons',
-                'buttons': ['galaxy', 'bega', 'sony']
-            },
-            "photo": {
-                "url": test.testPhoto.url,
-                "width": 640,
-                "height": 480
-            }
-        })
