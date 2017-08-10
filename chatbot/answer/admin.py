d05e382dfa297b3b35881bc4a0e4b6f1fd23ac36
yoonkt200@gmail.com <yoon@yungitaeui-MacBook-Pro.local>
2017-08-10 15:06:14 +0900

    admin add

--- chatbot/answer/admin.py
+++ chatbot/answer/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from answer.models import Maker, PhoneModel
+from answer.models import Maker, PhoneModel, Capacity
 
 
 class MakerAdmin(admin.ModelAdmin):
@@ -12,5 +12,11 @@ class PhoneModelAdmin(admin.ModelAdmin):
     list_display = ('modelName', 'created', 'modified')
 
 
+class CapacityAdmin(admin.ModelAdmin):
+    model = Capacity
+    list_display = ('modelGB', 'created', 'modified')
+
+
 admin.site.register(Maker, MakerAdmin)
 admin.site.register(PhoneModel, PhoneModelAdmin)
+admin.site.register(Capacity, CapacityAdmin)
