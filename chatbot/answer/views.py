c386085e919490534fea5d229741b6c4899f06e5
HJJ <orolgo@g.ucla.edu>
2017-08-10 13:53:17 +0900

    views.py

--- chatbot/answer/views.py
+++ chatbot/answer/views.py
@@ -43,64 +43,79 @@ def message(request):  # 버튼을 누르면 message 함수로 이동
 
     elif return_str == '도움말':
         help()
-    elif return_str == '삼성' | '애플' | 'LG' | '기타': # 제조사 선택
-        if return_str == '삼성':  # 응답에 대한 미러링과 버튼 제공
-            return JsonResponse({
-                'message': {
-                    'text': return_str + "를 선택하셨습니다. " + return_str + "의 상세 모델을 선택하여 주세요.",
-                },
 
-                # 시작하기/도움말 화면으로 이동(버튼)
-                'keyboard': {
-                    'type': 'buttons',
-                    'buttons': ['돌아가기'],
-                    # keyboard(request) 초기화면으로 돌아가는것 알아보기
-                },
-            })
+    elif return_str == '삼성':  # 응답에 대한 미러링과 버튼 제공
+        return JsonResponse({
+            'message': {
+                'text': return_str + "를 선택하셨습니다. " + return_str + "의 상세 모델을 선택하여 주세요.",
+            },
 
-        if return_str == '애플':  # 응답에 대한 미러링과 버튼 제공
-            return JsonResponse({
-                'message': {
-                    'text': return_str + "를 선택하셨습니다. " + return_str + "의 상세 모델을 선택하여 주세요.",
-                },
+            # 시작하기/도움말 화면으로 이동(버튼)
+            'keyboard': {
+                'type': 'buttons',
+                'buttons': ['돌아가기'],
+                # keyboard(request) 초기화면으로 돌아가는것 알아보기
+            },
+        })
 
-                # 시작하기/도움말 화면으로 이동(버튼)
-                'keyboard': {
-                    'type': 'buttons',
-                    'buttons': ['아이폰 5', '아이폰 6', '아이폰 7'],
-                    # keyboard(request) 초기화면으로 돌아가는것 알아보기
-                },
-            })
-        if return_str == 'LG':  # 응답에 대한 미러링과 버튼 제공
-            return JsonResponse({
-                'message': {
-                    'text': return_str + "를 선택하셨습니다. " + return_str + "의 상세 모델을 선택하여 주세요.",
-                },
+    elif return_str == '애플':  # 응답에 대한 미러링과 버튼 제공
+        return JsonResponse({
+            'message': {
+                'text': return_str + "를 선택하셨습니다. " + return_str + "의 상세 모델을 선택하여 주세요.",
+            },
 
-                # 시작하기/도움말 화면으로 이동(버튼)
-                'keyboard': {
-                    'type': 'buttons',
-                    'buttons': ['돌아가기'],
-                    # keyboard(request) 초기화면으로 돌아가는것 알아보기
-                },
-            })
-        if return_str == '기타':  # 응답에 대한 미러링과 버튼 제공
-            return JsonResponse({
-                'message': {
-                    'text': return_str + "를 선택하셨습니다. " + return_str + "의 상세 모델을 선택하여 주세요.",
-                },
+            # 시작하기/도움말 화면으로 이동(버튼)
+            'keyboard': {
+                'type': 'buttons',
+                'buttons': ['아이폰 5', '아이폰 6', '아이폰 7'],
+                # keyboard(request) 초기화면으로 돌아가는것 알아보기
+            },
+        })
+    elif return_str == 'LG':  # 응답에 대한 미러링과 버튼 제공
+        return JsonResponse({
+            'message': {
+                'text': return_str + "를 선택하셨습니다. " + return_str + "의 상세 모델을 선택하여 주세요.",
+            },
 
-                # 시작하기/도움말 화면으로 이동(버튼)
-                'keyboard': {
-                    'type': 'buttons',
-                    'buttons': ['돌아가기'],
-                    # keyboard(request) 초기화면으로 돌아가는것 알아보기
-                },
-            })
+            # 시작하기/도움말 화면으로 이동(버튼)
+            'keyboard': {
+                'type': 'buttons',
+                'buttons': ['돌아가기'],
+                # keyboard(request) 초기화면으로 돌아가는것 알아보기
+            },
+        })
+    elif return_str == '기타':  # 응답에 대한 미러링과 버튼 제공
+        return JsonResponse({
+            'message': {
+                'text': return_str + "를 선택하셨습니다. " + return_str + "의 상세 모델을 선택하여 주세요.",
+            },
+
+            # 시작하기/도움말 화면으로 이동(버튼)
+            'keyboard': {
+                'type': 'buttons',
+                'buttons': ['돌아가기'],
+                # keyboard(request) 초기화면으로 돌아가는것 알아보기
+            },
+        })
     elif return_str == '갤럭시 6' | '갤럭시 7' | '갤럭시 8': # 모델 선택
         check_is_model(return_str)
     else:
-        start(return_str)
+        return JsonResponse({
+
+            'message': {
+                'text': return_str,
+                "photo": {
+                    "url": "http://ec2-13-124-156-121.ap-northeast-2.compute.amazonaws.com" + test.testPhoto.url,
+                    # url을 image viewer url로 하면 더 깔끔할듯
+                    "width": 640,
+                    "height": 480
+                },
+            },
+
+            'keyboard': {
+                'type': 'text'
+            }
+        })
 
 def start(return_str):
     return JsonResponse({
@@ -134,7 +149,10 @@ def help(return_str):
 
 
 def check_is_model(return_str):  # 모델명 함수
-    return return_str
+    return JsonResponse({  # JSON타입으로 반환
+        'type': 'buttons',
+        'buttons': ['시작하기', '도움말']  # 채팅에 접속하면 2개의 버튼이 존재
+    })
 
     # 우선 버튼타입으로 제작 (아래 주석은 text 파일로 메시지를 입력받는 소스)
     '''
